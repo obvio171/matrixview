4dc78ba00f6a8bc7b12411f758ea7219ce3f8ad9
Helder Ribeiro <helder@gmail.com>
2008-10-05 05:46:24 -0300

    Non-contiguous multiple selection should be done with Ctrl.
    At least that's how it's done in Windows/Linux.

--- javascripts/matrixview.js
+++ javascripts/matrixview.js
@@ -4,17 +4,17 @@
 // For more information on this library, please see http://www.matrixview.org/.
 //
 // Copyright (c) 2007-2008 Justin Mecham <justin@aspect.net>
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -212,7 +212,7 @@ MatrixView.prototype = {
     }
 
     // Multiple Selection (Meta-Select)
-    else if (event && event.metaKey)
+    else if (event && event.ctrlKey)
     {
       // If the element is already selected, deselect it
       if (element.hasClassName('selected'))
@@ -322,7 +322,7 @@ MatrixView.prototype = {
     event.stop()
     element = $$('#' + this.element.id + ' li.selected').last()
     if (!element)
-      return this.selectFirst()    
+      return this.selectFirst()
     if (nextElement = element.next())
     {
       this.select(nextElement)
@@ -467,7 +467,7 @@ MatrixView.prototype = {
         }
       }
     )
- 
+
     // If a custom select handler has been defined, call it
     if (this.selectHandler != null)
       this.selectHandler(element)
